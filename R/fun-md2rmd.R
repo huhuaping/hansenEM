87e5c397482ffa6a2bffd5b3647483257d9fd5e3
huhuaping <huhuaping01@nwsuaf.edu.cn>
2022-09-21 12:03:07 +0800

    half pass

---
+++ R/fun-md2rmd.R
@@ -0,0 +1,31 @@
+
+# renv::install("tmcd82070/tex2rmd")
+# renv::install("huhuaping/tex2rmd")
+
+require("tex2rmd")
+require(stringr)
+
+files_tar <- list.files("mathpix/")
+qmd_tar <- str_replace_all(files_tar, "\\.tex","\\.qmd")
+paste0(qmd_tar,collapse = " ")
+
+
+input_md <- list.files("mathpix/",full.names = T)
+
+i <- 2
+for (i in 1:length(input_md)) {
+  
+  tex2rmd::tex2rmd(infile = input_md[i],
+          ext_out = ".qmd",
+          dir_img = "images/",
+          ext_img = ".jpg",
+          head2_only = TRUE, 
+          keep_yml =FALSE)
+  
+  cat(paste0("finished ",i," file convert ", input_md[i] ),
+      sep = "\n")
+}
+
+
+
+
---
+++ R/load-R-pkgs.R
@@ -0,0 +1,11 @@
+# base pkg
+require(knitr)
+require(tidyverse)
+require(here)
+require(glue)
+
+# data read and write
+require(openxlsx)
+
+# pkg and senssion maintain
+require(renv)
